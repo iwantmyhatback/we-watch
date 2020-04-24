a3c6b788ee77eec97827a92cf87b9aeeb94401d4
Tristan Deitzer <tristandeitzer@gmail.com>
2020-04-24 12:35:23 -0400

    expensive syncing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Videos from './Videos.js';
 import axios from 'axios';
+import io from 'socket.io-client';
 
 class App extends React.Component {
   constructor(props) {
@@ -14,24 +15,83 @@ class App extends React.Component {
     this.newVideo = this.newVideo.bind(this);
     this.setListeners = this.setListeners.bind(this);
     this.removeListeners = this.removeListeners.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.handleSeek = this.handleSeek.bind(this);
+    this.handlePlay = this.handlePlay.bind(this);
+    this.handlePause = this.handleSeek.bind(this);
+    this.sync = this.sync.bind(this);
+  }
+
+  sync() {
+    const socket = io.connect('http://localhost');
+    socket.on('check-request', (data) => {
+      console.log('RECIEVED CHECK-REQUEST');
+      if (data.current === null) {
+        console.log('EMITTED CHECK-RESPONSE');
+        socket.emit('check-response', {
+          current: this.state.current,
+          seconds: this.state.seconds,
+          play: this.state.play,
+        });
+      } else {
+        document.getElementById('videoPlayer').currentTime = data.seconds;
+        if (data.play) {
+          document.getElementById('videoPlayer').play();
+        } else {
+          document.getElementById('videoPlayer').pause();
+        }
+      }
+    });
   }
 
   setListeners() {
-    document.getElementById('videoPlayer').addEventListener('seeking', this.handleChange);
-    document.getElementById('videoPlayer').addEventListener('play', this.handleChange);
-    document.getElementById('videoPlayer').addEventListener('pause', this.handleChange);
+    document.getElementById('videoPlayer').addEventListener('seeking', this.handleSeek);
+    document.getElementById('videoPlayer').addEventListener('play', this.handlePlay);
+    document.getElementById('videoPlayer').addEventListener('pause', this.handlePause);
+    document.getElementById('videoPlayer').addEventListener('timeupdate', this.handlePause);
   }
 
   removeListeners() {
-    document.getElementById('videoPlayer').removeEventListener('seeking', this.handleChange);
-    document.getElementById('videoPlayer').removeEventListener('play', this.handleChange);
-    document.getElementById('videoPlayer').removeEventListener('pause', this.handleChange);
+    document.getElementById('videoPlayer').removeEventListener('seeking', this.handleSeek);
+    document.getElementById('videoPlayer').removeEventListener('play', this.handlePlay);
+    document.getElementById('videoPlayer').removeEventListener('pause', this.handlePause);
+    document.getElementById('videoPlayer').removeEventListener('timeupdate', this.handlePause);
   }
 
-  handleChange(event) {
-    console.log(event.target.currentTime);
+  handleTimeupdate(event) {
+    console.log('timeupdate');
+  }
+
+  handleSeek(event) {
+    // console.log(event.target.currentTime);
     this.setState({ seconds: event.target.currentTime });
+    const socket = io.connect('http://localhost');
+    socket.emit('playing', {
+      current: this.state.current,
+      seconds: this.state.seconds + 0.9,
+      play: this.state.play,
+    });
+  }
+
+  handlePlay(event) {
+    console.log(event.target.currentTime);
+    this.setState({ seconds: event.target.currentTime, play: true });
+    const socket = io.connect('http://localhost');
+    socket.emit('playing', {
+      current: this.state.current,
+      seconds: this.state.seconds + 0.9,
+      play: this.state.play,
+    });
+  }
+
+  handlePause(event) {
+    console.log(event.target.currentTime);
+    this.setState({ seconds: event.target.currentTime, play: false });
+    const socket = io.connect('http://localhost');
+    socket.emit('playing', {
+      current: this.state.current,
+      seconds: this.state.seconds + 0.9,
+      play: this.state.play,
+    });
   }
 
   componentDidUpdate() {
@@ -44,6 +104,7 @@ class App extends React.Component {
 
   componentDidMount() {
     this.setListeners();
+    this.sync();
   }
 
   newVideo(newVideo) {
